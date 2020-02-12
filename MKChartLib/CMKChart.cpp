012ad006acd348c822d5464f780f71795bb99a45
Kobayashi <masato.kobayashi@shi-g.com>
2020-02-12 20:16:33 +0900

    update20200212

--- MKChartLib/CMKChart.cpp
+++ MKChartLib/CMKChart.cpp
@@ -27,39 +27,278 @@ namespace MKChart {
 		//CHART 1
 		DWORD	str_num = GetPrivateProfileString(SET1_SECT_OF_INIFILE, SET_CHART_X_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 4) mkchartset[MK_CHART1].chart_x = _wtoi(buf_t);
 				str_num = GetPrivateProfileString(SET1_SECT_OF_INIFILE, SET_CHART_Y_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 4) mkchartset[MK_CHART1].chart_y = _wtoi(buf_t);
-#if 0
-		//LOG01
-		if (nlogs > 0) {
-			str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGNAME1_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].fname, sizeof(mklogset[MK_LOGSET_1].fname), szInipath);
-			_wsplitpath_s(szInipath, szDrive, sizeof(szDrive), szPath, sizeof(szPath), szFName, sizeof(szFName), szExt, sizeof(szExt)); // exe fileのパス分割
-																																		//	_wmakepath_s(mklogset[MK_LOGSET_1].fname, sizeof(mklogset[MK_LOGSET_1].fname), szDrive, szPath, buf_t, EXT_OF_CSVFILE);
-			str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_LOGTYPE1_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].szType, sizeof(mklogset[MK_LOGSET_1].szType), szInipath);
-			str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_SAMPLE_TIME1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].ms_period = stoi(buf_t);
-			str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_NUM1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_1].n_item = stoi(buf_t);
-			str_num = GetPrivateProfileString(SET_SECT_OF_INIFILE, SET_ITME_TIMESET1_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5) mklogset[MK_LOGSET_1].timeset = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE01_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[0] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE02_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[1] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE03_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[2] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE04_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[3] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE05_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[4] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE06_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[5] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE07_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[6] = stoi(buf_t);
-			str_num = GetPrivateProfileString(DATA_TYPE1_SECT_OF_INIFILE, TYPE08_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 5)mklogset[MK_LOGSET_1].data_type[7] = stoi(buf_t);
+				str_num = GetPrivateProfileString(SET1_SECT_OF_INIFILE, SET_CHART_W_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 4) mkchartset[MK_CHART1].chart_w = _wtoi(buf_t);
+				str_num = GetPrivateProfileString(SET1_SECT_OF_INIFILE, SET_CHART_H_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 4) mkchartset[MK_CHART1].chart_h = _wtoi(buf_t);
+				str_num = GetPrivateProfileString(SET1_SECT_OF_INIFILE, SET_CHART_PLOT_MS_KEY_OF_INIFILE, L"FAILED", buf_t, sizeof(buf_t), szInipath); if (str_num < 4) mkchartset[MK_CHART1].plot_interval_ms = _wtoi(buf_t);
 
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL01_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[0], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL02_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[1], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL03_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[2], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL04_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[3], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL05_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[4], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL06_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[5], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL07_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[6], LABEL_LEN_MAX, szInipath);
-			str_num = GetPrivateProfileString(LABEL_NAME1_SECT_OF_INIFILE, LABEL08_KEY_OF_INIFILE, L"FAILED", mklogset[MK_LOGSET_1].labes[7], LABEL_LEN_MAX, szInipath);
-
-		}
-#endif	
 	return 0;
 	};
+	//########################################################################
+	int CMKChart::open_chart(int chartID, HWND hwnd_parent) {
+		HWND hwnd;
+	
+
+		WNDCLASSEX wc;
+
+		HINSTANCE hInstance = GetModuleHandle(0);
+
+		ZeroMemory(&wc, sizeof(wc));
+		wc.cbSize = sizeof(WNDCLASSEX);
+
+		wc.style = CS_HREDRAW | CS_VREDRAW;
+		wc.lpfnWndProc = ChartWndProc;// !CALLBACKでreturnを返していないとWindowClassの登録に失敗する
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
+		wc.hInstance = hInstance;
+		wc.hIcon = NULL;
+		wc.hCursor = LoadCursor(0, IDC_ARROW);
+		wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+		wc.lpszMenuName = NULL;
+		wc.lpszClassName = TEXT("myChart");
+		wc.hIconSm = NULL;
+		ATOM fb = RegisterClassExW(&wc);
+
+		mkchartset[chartID].hwnd_chart = CreateWindow(TEXT("myChart"),
+			TEXT("MY CHART"),
+			WS_POPUPWINDOW | WS_VISIBLE | WS_CAPTION,
+			CHART_WND_X,
+			CHART_WND_Y,
+			CHART_WND_W,
+			CHART_WND_H,
+			hwnd_parent,
+			0,
+			hInstance,
+			NULL);
+
+		ShowWindow(mkchartset[chartID].hwnd_chart, SW_SHOW);
+		UpdateWindow(mkchartset[chartID].hwnd_chart);
+		return 0;
+	};
+#if 0
+	LRESULT CMKChart::ChartWndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+
+		BITMAP bmp_info;
+		static HWND hinfchk;
+		int nFramX, nFramY, nCaption, scrw, scrh, winw, winh, winx, winy, mob_speed = 4;
+		HDC hdc;
+		TCHAR szBuf[128];
+		HMENU hsubmenu, hpopmenu;
+		HWND tmpwnd;
+
+
+		switch (msg) {
+		case WM_CREATE: {
+
+			nFramX = GetSystemMetrics(SM_CXSIZEFRAME);		//ウィンドウ周囲の幅
+			nFramY = GetSystemMetrics(SM_CYSIZEFRAME);		//ウィンドウ周囲の高さ
+			nCaption = GetSystemMetrics(SM_CYCAPTION);		//タイトルバーの高さ
+			scrw = GetSystemMetrics(SM_CXSCREEN);			//プライマモニタの幅
+			scrh = GetSystemMetrics(SM_CYSCREEN);			//プライマモニタの高さ
+/*
+			hwnd_startPB = CreateWindow(
+				L"BUTTON", L"Start",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				CHART_WND_W - 100, CHART_WND_H - 70, 38, 25, hWnd, (HMENU)IDC_CHART_START_PB, Chart::hInst, NULL);
+			hwnd_stopPB = CreateWindow(
+				L"BUTTON", L"Stop",
+				WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+				CHART_WND_W - 58, CHART_WND_H - 70, 38, 25, hWnd, (HMENU)IDC_CHART_STOP_PB, Chart::hInst, NULL);
+			hwnd_radio_g = CreateWindow(
+				L"BUTTON", L"graph",
+				WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON,
+				CHART_WND_W - 150, 5, 60, 25, hWnd, (HMENU)IDC_CHART_RADIO_GRAPH, Chart::hInst, NULL);
+			hwnd_radio_p = CreateWindow(
+				L"BUTTON", L"phase",
+				WS_CHILD | WS_VISIBLE | BS_RADIOBUTTON,
+				CHART_WND_W - 80, 5, 60, 25, hWnd, (HMENU)IDC_CHART_RADIO_PHASE, Chart::hInst, NULL);
 
 
 
+			init_chart();
+
+			hdc = GetDC(hWnd);
+			HBITMAP hDummy = CreateCompatibleBitmap(hdc, st_disp.chart_w, st_disp.chart_h);
+			SelectObject(st_disp.hdc_mem0, hDummy);		//合成画像（背景＋グラフ）
+			hDummy = CreateCompatibleBitmap(hdc, st_disp.chart_w, st_disp.chart_h);
+			SelectObject(st_disp.hdc_mem_bg, hDummy);	//背景画像
+			hDummy = CreateCompatibleBitmap(hdc, st_disp.chart_w, st_disp.chart_h);
+			SelectObject(st_disp.hdc_mem_graph, hDummy);//グラフ画像
+
+			PatBlt(st_disp.hdc_mem0, 0, 0, st_disp.chart_w, st_disp.chart_h, WHITENESS);//HDC hdc, int x,int y, int w, int h,DWORD rop
+			PatBlt(st_disp.hdc_mem_bg, 0, 0, st_disp.chart_w, st_disp.chart_h, WHITENESS);//HDC hdc, int x,int y, int w, int h,DWORD rop
+			PatBlt(st_disp.hdc_mem_graph, 0, 0, st_disp.chart_w, st_disp.chart_h, WHITENESS);//HDC hdc, int x,int y, int w, int h,DWORD rop
+
+			ReleaseDC(hWnd, hdc);
+			DeleteObject(hDummy);
+
+			//SelectObject(st_disp.hdc_mem_bg, GetStockObject(BLACK_PEN));
+
+			POINT linkpt[2];
+			POINT toPT;
+
+			SelectObject(st_disp.hdc_mem_bg, GetStockObject(DC_PEN));
+			SetDCPenColor(st_disp.hdc_mem_bg, RGB(128, 128, 128));
+			for (int i = 0; i < CHART_NUM; i++) {
+				MoveToEx(st_disp.hdc_mem_bg, st_disp.g_origin[i].x, st_disp.g_origin[i].y, NULL);
+				LineTo(st_disp.hdc_mem_bg, st_disp.g_origin[i].x + CHART_DOT_W, st_disp.g_origin[i].y);
+
+				MoveToEx(st_disp.hdc_mem_bg, st_disp.g_origin[i].x, st_disp.g_origin[i].y + CHART_DOT_H / 2 - 5, NULL);
+				LineTo(st_disp.hdc_mem_bg, st_disp.g_origin[i].x, st_disp.g_origin[i].y - CHART_DOT_H / 2 + 5);
+
+			}
+
+			//TransparentBlt(st_disp.hdc_mem0, 0, 0, st_disp.chart_w, st_disp.chart_h, st_disp.hdc_mem_graph, 0, 0, st_disp.chart_w, st_disp.chart_h, RGB(255, 255, 255));
+			//BitBlt(st_disp.hdc_mem0, 0, 0, st_disp.chart_w, st_disp.chart_h, st_disp.hdc_mem_bg, 0, 0, SRCCOPY);
+*/
+
+		}break;
+
+		case WM_TIMER: {
+			std::wcout << "Timer Triggerred from Lib";
+/*
+			if (st_disp.disp_type == CHART_GRAPH) {
+
+				st_disp.g_write_line += st_disp.g_ms_per_dot * st_disp.plot_interval_ms;
+				if (st_disp.g_write_line >= CHART_WND_W * 2) {
+					st_disp.g_write_line -= CHART_WND_W * 2;
+				}
+				int iline = 1;
+				double tem_d = 0.1;
+				plot_graph(iline, &tem_d);
+			}
+			else if (st_disp.disp_type == CHART_PHASE) {
+				;
+			}
+			else;
+			InvalidateRect(Chart::hwnd_chart, NULL, FALSE);
+		}break;
+		case WM_COMMAND:
+			switch (LOWORD(wp)) {
+			case IDC_CHART_START_PB:
+				SetTimer(hwnd_chart, ID_CHART_TIMER, Chart::chart_interval_ms, NULL);
+				break;
+			case IDC_CHART_STOP_PB:
+				KillTimer(hwnd_chart, ID_CHART_TIMER);
+				break;
+			case IDC_CHART_RADIO_GRAPH:
+				st_disp.disp_type = CHART_GRAPH;
+				SendMessage(hwnd_radio_g, BM_SETCHECK, BST_CHECKED, 0L);
+				SendMessage(hwnd_radio_p, BM_SETCHECK, BST_UNCHECKED, 0L);
+				break;
+			case IDC_CHART_RADIO_PHASE:
+				st_disp.disp_type = CHART_PHASE;
+				SendMessage(hwnd_radio_p, BM_SETCHECK, BST_CHECKED, 0L);
+				SendMessage(hwnd_radio_g, BM_SETCHECK, BST_UNCHECKED, 0L);
+				break;
+
+				*/
+			}break;
+		case WM_PAINT: {
+			PAINTSTRUCT ps;
+/*
+			hdc = BeginPaint(hWnd, &ps);
+			TransparentBlt(st_disp.hdc_mem0, 0, 0, st_disp.chart_w, st_disp.chart_h, st_disp.hdc_mem_bg, 0, 0, st_disp.chart_w, st_disp.chart_h, RGB(255, 255, 255));
+			TransparentBlt(st_disp.hdc_mem0, 0, 0, st_disp.chart_w, st_disp.chart_h, st_disp.hdc_mem_graph, 0, 0, st_disp.chart_w, st_disp.chart_h, RGB(255, 255, 255));
+
+			BitBlt(
+				hdc,				//HDC hdc
+				0,					//int x
+				0,					//int y
+				st_disp.chart_w,	//int cx
+				st_disp.chart_h,	//int c
+				st_disp.hdc_mem0,	//HDC hdcSrc
+				0,					//int x1
+				0,					//int y1
+				SRCCOPY//DWORD rop
+			);
+			EndPaint(hWnd, &ps);
+*/
+			break;
+		}
+		case WM_CLOSE:
+			DestroyWindow(hWnd);
+			break;
+		case WM_KEYDOWN:
+			if (wp == 'Q') {
+				DestroyWindow(hWnd);
+			}
+			break;
+		default:
+			return DefWindowProc(hWnd, msg, wp, lp);
+		}
+		return 0;
+	}
+#endif
+	LRESULT CMKChart::ChartWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+		HDC hdc;
+
+		switch (msg) {
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		case WM_TIMER:
+			hdc = GetDC(hwnd);
+			TextOut(hdc, 0, 0, L"Test\0", sizeof("Test\0"));
+			return 0;
+		case WM_CREATE:
+
+			return 0;
+		}
+		return DefWindowProc(hwnd, msg, wp, lp);
+	}
+
+
+	//########################################################################
+	int CMKChart::set_double_data(double* pd, int chartID, int i_item, bool is_x, double d_100) {
+		if ((chartID < 0) || (chartID >= MK_CHART_MAX))return - 1;
+		if ((i_item < 0) || (i_item >= MK_MAX_GRAPH_PER_CHART))return -1;
+
+		if (is_x) {
+			mkchartset[chartID].pdata[i_item][MK_DATA_CODE_X][MK_ADDRESS_DOUBLE] = pd;
+			mkchartset[chartID].value100->d100_x = d_100;
+			mkchartset[chartID].data_type[i_item][MK_DATA_CODE_X] = MK_DATA_TYPE_DOUBLE;
+		}
+		else {
+			mkchartset[chartID].pdata[i_item][MK_DATA_CODE_Y][MK_ADDRESS_DOUBLE] = pd;
+			mkchartset[chartID].value100->d100_y = d_100;
+			mkchartset[chartID].data_type[i_item][MK_DATA_CODE_Y] = MK_DATA_TYPE_DOUBLE;
+		}
+		return 0;
+	};
+	//########################################################################
+	int CMKChart::set_int_data(int* pi, int chartID, int i_item, bool is_x, int i_100) {
+		if ((chartID < 0) || (chartID >= MK_CHART_MAX))return -1;
+		if ((i_item < 0) || (i_item >= MK_MAX_GRAPH_PER_CHART))return -1;
+
+		if (is_x) {
+			mkchartset[chartID].pdata[i_item][MK_DATA_CODE_X][MK_ADDRESS_INT] = pi;
+			mkchartset[chartID].value100->i100_x = i_100;
+			mkchartset[chartID].data_type[i_item][MK_DATA_CODE_X] = MK_DATA_TYPE_INT;
+		}
+		else {
+			mkchartset[chartID].pdata[i_item][MK_DATA_CODE_Y][MK_ADDRESS_INT] = pi;
+			mkchartset[chartID].value100->i100_y = i_100;
+			mkchartset[chartID].data_type[i_item][MK_DATA_CODE_Y] = MK_DATA_TYPE_INT;
+		}
+		if ((chartID < 0) || (chartID >= MK_CHART_MAX))return -1;
+		if ((i_item < 0) || (i_item >= MK_MAX_GRAPH_PER_CHART))return -1;
+		return 0;
+	};
+	//########################################################################
+	int CMKChart::set_bool_data(bool* pb, int chartID, int i_item, int i_bool) {
+		if ((chartID < 0) || (chartID >= MK_CHART_MAX))return -1;
+		if ((i_item < 0) || (i_item >= MK_MAX_GRAPH_PER_CHART))return -1;
+		if ((i_bool < 0) || (i_item >= MK_ADDRESS_BOOL_MAX))return -1;
+
+		mkchartset[chartID].pdata[i_item][MK_DATA_CODE_X][i_bool] = nullptr; //BOOLのX軸は時間のみ
+		mkchartset[chartID].value100->i100_x = 100000;
+		mkchartset[chartID].data_type[i_item][MK_DATA_CODE_X] = MK_DATA_TYPE_TIME;
+
+		mkchartset[chartID].pdata[i_item][MK_DATA_CODE_Y][i_bool] = pb;
+		mkchartset[chartID].value100->i100_y = 1;
+		mkchartset[chartID].data_type[i_item][MK_DATA_CODE_Y] = MK_DATA_TYPE_BOOL;
+
+		return 0;
+	};
+	//########################################################################
 }
--- MKChartLib/CMKChart.h
+++ MKChartLib/CMKChart.h
@@ -18,15 +18,39 @@ namespace MKChart
 #define MK_CHART3				2
 #define MK_CHART4				3
 
+#define MK_DATA_TYPE_TIME		0
+#define MK_DATA_TYPE_DOUBLE		1
+#define MK_DATA_TYPE_INT		2
+#define MK_DATA_TYPE_BOOL		3
+
 #define MK_MAX_GRAPH_PER_CHART	4
 #define MK_CHART_BUF_MAX		600
 #define MK_RANGE_100PERCENT		100000
 
+#define MK_DATA_CODE_X			0
+#define MK_DATA_CODE_Y			1
+#define MK_ADDRESS_INT			0
+#define MK_ADDRESS_DOUBLE		0
+#define MK_ADDRESS_BOOL_MAX		4
+#define MK_ADDRESS_BOOL1		0
+#define MK_ADDRESS_BOOL2		1
+#define MK_ADDRESS_BOOL3		2
+#define MK_ADDRESS_BOOL4		3
+
+
+
+
 	typedef struct _stRANGE_SET
 	{
-		double value_x;
-		double value_y;
+		double d100_x;
+		double d100_y;
+		int i100_x;
+		int i100_y;
 	}ST_RANGE_SET, *LPST_RANGE_SET;
+	union MK_plot {
+		INT32 i_data;
+		bool b_data[4];
+	};
 
 	typedef struct _stCHART_SET
 	{
@@ -37,12 +61,16 @@ namespace MKChart
 		int refresh_interval_ms;				//グラフ表示クリア間隔msec
 		DWORD	start_time_ms;					//チャート開始時間
 		DWORD	latast_plot_time_ms;			//最後にプロットした時間
-		INT32	plot_data[MK_MAX_GRAPH_PER_CHART][MK_CHART_BUF_MAX];//チャートプロットバッファ100%=100000
+		MK_plot	plot_data[MK_MAX_GRAPH_PER_CHART][MK_CHART_BUF_MAX];//チャートプロットバッファ100%=100000
 		ST_RANGE_SET	value100[MK_MAX_GRAPH_PER_CHART];//プロットデータ100%の値　時間は、100秒=100%とする
+
 		POINT g_origin[MK_MAX_GRAPH_PER_CHART];		//グラフの原点
 		int dot100percent_x, dot100percent_y;//100%に割り当てるdot数
 
+		void* pdata[MK_MAX_GRAPH_PER_CHART][2][4];		//プロットデータのアドレス　NULL->時間
+		int	data_type[MK_MAX_GRAPH_PER_CHART][2];	//プロットデータの型
 
+		HWND hwnd_chart;				//チャートウィンドウのハンドル
 		HDC hdc_mem0;					//合成画面メモリデバイスコンテキスト
 		HDC hdc_mem_bg;					//背景画面メモリデバイスコンテキスト
 		HDC hdc_mem_graph;				//グラフ画面メモリデバイスコンテキスト
@@ -70,13 +98,20 @@ namespace MKChart
 		static	WCHAR szInipath[_MAX_PATH], szDrive[_MAX_DRIVE], szPath[_MAX_PATH], szFName[_MAX_FNAME], szExt[_MAX_EXT];//iniファイルパス取得用
 		static	WCHAR szLogfolder1[_MAX_PATH];
 		static  std::wstring log_path;
-
 		static	ST_CHART_SET mkchartset[MK_CHART_MAX];
+	//	static HINSTANCE hInst;
+
 
 		static	int init_chartfunc();
-		static	int chart_start(int chart_id);
-		static	int chart_stop(int chart_id);
+		static	int set_double_data(double* pd, int chartID, int i_item,bool is_x, double d_100);
+		static	int set_int_data(int* pi, int chartID, int i_item, bool is_x, int i_100);
+		static	int set_bool_data(bool* pb, int chartID, int i_item, int i_bool);
+
+		static int open_chart(int chartID, HWND hwnd_parent);
+		static LRESULT CALLBACK ChartWndProc(HWND, UINT, WPARAM, LPARAM);
 		
+		static	int chart_start(int chartID, int chart_id);
+		static	int chart_stop(int chartID, int chart_id);
 
 	};
 }
